c9c284f65d1a6fba41cabbeecbbb28bd2564ebe7
sadra amlashi <amlashi.sadra@gmail.com>
2017-06-29 00:03:39 +0430

    push with jekgit

---
+++ jekgit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+  echo "Please enter a git commit message"
+  exit
+fi
+
+jekyll build && \
+  git add . && \
+  git commit -am "$1" && \
+  git push origin master && \
+  cd _site && \
+  git add . && \
+  git commit -am "$1" && \
+  git push origin master && \
+  cd .. && \
+  echo "Successfully built and pushed to GitHub."

c98b24fbe5574ea4008b8877ddb49531846f6853
sadra amlashi <amlashi.sadra@gmail.com>
2017-06-29 22:19:59 +0430

    Fix not-loaded video link

--- _plugins/Youtube.rb
+++ _plugins/Youtube.rb
@@ -9,7 +9,7 @@ module Jekyll
 
       youtube_id = @input.strip!
 
-      output =  "<a href='https://www.youtube.com/watch?v="+youtube_id+"' target='_blank'><div class='video-container'><style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://www.youtube.com/embed/"+youtube_id+"' frameborder='0' allowfullscreen></iframe></div></div><a/>"
+      output =  "<div class='video-container'><style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://www.youtube.com/embed/"+youtube_id+"' frameborder='0' allowfullscreen></iframe></div></div>"
 
       return output;
     end

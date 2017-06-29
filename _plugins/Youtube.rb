ecdb6641737562da82c1eb18194090b53d9364f8
sadra amlashi <amlashi.sadra@gmail.com>
2017-06-29 22:24:09 +0430

    Fix not-loaded video ssl links

--- _plugins/Youtube.rb
+++ _plugins/Youtube.rb
@@ -6,7 +6,6 @@ module Jekyll
     end
 
     def render(context)
-
       youtube_id = @input.strip!
 
       output =  "<div class='video-container'><style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/"+youtube_id+"' frameborder='0' allowfullscreen></iframe></div></div>"

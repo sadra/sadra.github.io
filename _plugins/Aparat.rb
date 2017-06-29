25ad74d33291faa4106a9d158db2f9b07f7a306b
sadra amlashi <amlashi.sadra@gmail.com>
2017-06-29 22:10:37 +0430

    Add link not-loaded video background

--- _plugins/Aparat.rb
+++ _plugins/Aparat.rb
@@ -9,7 +9,7 @@ module Jekyll
 
       aparat_id = @input.strip!
 
-      output =  '<div class="video-container"><style>.h_iframe-aparat_embed_frame{position:relative;} .h_iframe-aparat_embed_frame .ratio {display:block;width:100%;height:auto;} .h_iframe-aparat_embed_frame iframe {position:absolute;top:0;left:0;width:100%; height:100%;}</style><div class="h_iframe-aparat_embed_frame"> <span style="display: block;padding-top: 57%"></span><iframe src="https://www.aparat.com/video/video/embed/videohash/'+aparat_id+'/vt/frame" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" ></iframe></div></div>'
+      output =  '<a href="http://www.aparat.com/v/'+aparat_id+'"><div class="video-container"><style>.h_iframe-aparat_embed_frame{position:relative;} .h_iframe-aparat_embed_frame .ratio {display:block;width:100%;height:auto;} .h_iframe-aparat_embed_frame iframe {position:absolute;top:0;left:0;width:100%; height:100%;}</style><div class="h_iframe-aparat_embed_frame"> <span style="display: block;padding-top: 57%"></span><iframe src="https://www.aparat.com/video/video/embed/videohash/'+aparat_id+'/vt/frame" allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" ></iframe></div></div></a>'
 
       return output;
     end
--- _plugins/Youtube.rb
+++ _plugins/Youtube.rb
@@ -7,9 +7,9 @@ module Jekyll
 
     def render(context)
 
-      aparat_id = @input.strip!
+      youtube_id = @input.strip!
 
-      output =  "<div class='video-container'><style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://www.youtube.com/embed/"+aparat_id+"' frameborder='0' allowfullscreen></iframe></div></div>"
+      output =  "<a href='https://www.youtube.com/watch?v="+youtube_id+"'><div class='video-container'><style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://www.youtube.com/embed/"+youtube_id+"' frameborder='0' allowfullscreen></iframe></div></div><a/>"
 
       return output;
     end
